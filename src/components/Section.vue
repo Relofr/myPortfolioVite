675138c8316a545ebeaaa0c6f145cdfde1484659
Kyle Hatch <ky13h4tch@gmail.com>
2022-07-06 13:01:08 -0600

    Created a dynamic section component

---
+++ src/components/Section.vue
@@ -0,0 +1,64 @@
+<template>
+    <div :class="bgColor + ' ' + textColor + ' ' + orientation">
+        <p>{{ title }}</p>
+        <div class="split">
+            <div>
+                <p>{{ leftContent }}</p>
+            </div>
+            <div>
+                <p>{{ rightContent }}</p>
+            </div>
+            <div v-if="imageSrc">
+                <img :src="`/src/assets/images/` + imageSrc" :alt="alt">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        bgColor: {
+            type: String,
+            required: true
+        },
+        textColor: {
+            type: String,
+            required: true
+        },
+        orientation: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        leftContent: {
+            type: String,
+            required: true
+        },
+        rightContent: {
+            type: String,
+            required: true
+        },
+        imageSrc: {
+            type: String,
+            required: true
+        },
+        alt: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+.horizontal {
+    
+}
+.vertical {
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <script setup>
-import HomeComponent from '../components/HomeComponent.vue';
+import Section from '../components/Section.vue';
 </script>
 
 <template>
-    <HomeComponent />
+    <Section 
+        bgColor="bg-primary-400" 
+        textColor="text-accent-400" 
+        orientation="horizontal"
+        title="Hello" 
+        leftContent="left content" 
+        rightContent="right content" 
+        imageSrc="ProfilePicture.jpeg"
+    />
 </template>
