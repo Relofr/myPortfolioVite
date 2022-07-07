b1465e3558d86dc424d6c2a01811ec9cc14aaf7a
Kyle Hatch <ky13h4tch@gmail.com>
2022-07-06 19:34:12 -0600

    Added a sectionBreak component

---
+++ src/components/SectionBreak.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="sectionBlock">
+        {{ title }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+.sectionBlock {
+    display: flex;
+    width: 100%;
+    height: 120px;
+    background-color: var(--clr-primary-400);
+    color: var(--clr-accent-100);
+    font-size: var(--fs-800);
+    border-radius: 5px;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+    margin: 80px 0px 80px 0px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup>
 import Section from '../components/Section.vue';
+import SectionBreak from '../components/SectionBreak.vue';
 </script>
 
 <template>
     <Section bgColor="bg-accent-400" fontColor="text-accent-100" fontSize="fs-secondary-heading" title="Millicent"
         leftContent="Is my favorite" rightContent="Is very cool" />
+
+    <SectionBreak title="Design"/>
 
     <Section bgColor="bg-secondary-400" fontColor="text-primary-100" fontSize="fs-secondary-heading" title="Me"
         leftContent="HELLO" rightContent="HI" />
