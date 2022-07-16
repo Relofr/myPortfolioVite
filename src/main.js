bf1f209e955d18a70293af43e8e40263c3b915c1
Kyle Hatch <ky13h4tch@gmail.com>
2022-07-16 17:05:12 -0600

    compressed images

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -7,7 +8,6 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fas, fab);
 
-import router from './router'
 
 createApp(App)
     .component('fa', FontAwesomeIcon)
